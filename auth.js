e35e0602784e66f8aa94bd377efd89b4fc2f7ebe
Dhanraj18-art <dhanrajbharat6@gmail.com>
2026-03-20 19:18:24 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,49 +1,4 @@
-// SIGNUP
-async function signup() {
 
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-
-    const { data, error } = await supabaseClient.auth.signUp({
-        email: email,
-        password: password
-    })
-
-    console.log("Signup Data:", data)
-    console.log("Signup Error:", error)
-
-    if (error) {
-        alert("❌ " + error.message)
-    } else {
-        alert("✅ Signup Successful")
-
-        // redirect to login
-        window.location.href = "login.html"
-    }
-}
-
-
-// LOGIN
-async function login() {
-
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-
-    const { data, error } = await supabaseClient.auth.signInWithPassword({
-        email: email,
-        password: password
-    })
-
-    console.log("DATA:", data)
-    console.log("ERROR:", error)
-
-    if (error) {
-        alert("❌ " + error.message)
-    } else {
-        alert("✅ Login Success")
-        window.location.href = "dashboard.html"
-    }
-}
 
 // LOGOUT
 async function logout() {
